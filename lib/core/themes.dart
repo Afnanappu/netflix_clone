3dc0f5585e87f2e468e435a57ab486228fb3a49e
AFNAN P <afnanp860@gmail.com>
2024-10-30 11:56:53 +0530

    bottom navgation ui

---
+++ lib/core/themes.dart
@@ -0,0 +1,8 @@
+
+
+import 'package:flutter/material.dart';
+import 'package:netflix/core/colors/colors.dart';
+
+class MyTextTheme{
+  static const lightGreyFont =  TextStyle(color: MyColors.lightGrey, fontSize: 18,);
+}
---
+++ lib/presentation/news_and_hot/screen_news_and_hot.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class ScreenNewsAndHot extends StatelessWidget {
+  const ScreenNewsAndHot({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(child: Text('News and Hot screen'),),
+    );
+  }
+}
