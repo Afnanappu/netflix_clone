d5c5ea45a22ad66851e8629130c7638304feb45c
AFNAN P <afnanp860@gmail.com>
2024-11-01 15:27:28 +0530

    trying to change laptop

--- lib/presentation/news_and_hot/screen_news_and_hot.dart
+++ lib/presentation/news_and_hot/screen_news_and_hot.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:netflix/core/colors/colors.dart';
-import 'package:netflix/presentation/home/widgets/elevated_button_for_home.dart';
 import 'package:netflix/presentation/my_netflix/widgets/my_netflix_app_bar.dart';
 
 class ScreenNewsAndHot extends StatelessWidget {
@@ -18,52 +17,56 @@ class ScreenNewsAndHot extends StatelessWidget {
         length: 2,
         initialIndex: 0,
         child: TabBar(
-dividerColor: Colors.black,         
-automaticIndicatorColorAdjustment: false, 
-indicator: UnderlineTabIndicator(borderSide: BorderSide(color: MyColors.transparent)),
-          tabs: [
-          Tab(
-              child: ElevatedButton.icon(
-            onPressed: () {},
-            label: const Text(
-              'Coming Soon',
-              style: TextStyle(
-                color: MyColors.black,
-                fontFamily: "Netflix_Sans",
-                fontWeight: FontWeight.w700,
+            dividerColor: Colors.black,
+            automaticIndicatorColorAdjustment: false,
+            indicator: const UnderlineTabIndicator(
+              borderSide: BorderSide(
+                color: MyColors.transparent,
               ),
             ),
-            icon: const Icon(Icons.fastfood_outlined),
-            style: ElevatedButton.styleFrom(
-              backgroundColor: MyColors.white,
-            ),
-          )),
-          Tab(
-              child: ElevatedButton.icon(
-            onPressed: () {},
-            label: const Text(
-              'Coming Soon',
-              style: TextStyle(
-                color: MyColors.black,
-                fontFamily: "Netflix_Sans",
-                fontWeight: FontWeight.w700,
-              ),
-            ),
-            icon: const Icon(Icons.fastfood_outlined),
-            style: ElevatedButton.styleFrom(
-              backgroundColor: MyColors.white,
-            ),
-          )),
-          // Tab(
-          //   child: Row(
-          //     mainAxisAlignment: MainAxisAlignment.center,
-          //     children: [
-          //       Icon(Icons.abc),
-          //       Text('data'),
-          //     ],
-          //   ),
-          // ),
-        ]),
+            tabs: [
+              Tab(
+                  child: ElevatedButton.icon(
+                onPressed: () {},
+                label: const Text(
+                  'Coming Soon',
+                  style: TextStyle(
+                    color: MyColors.black,
+                    fontFamily: "Netflix_Sans",
+                    fontWeight: FontWeight.w700,
+                  ),
+                ),
+                icon: const Icon(Icons.fastfood_outlined),
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: MyColors.white,
+                ),
+              )),
+              Tab(
+                  child: ElevatedButton.icon(
+                onPressed: () {},
+                label: const Text(
+                  'Coming Soon',
+                  style: TextStyle(
+                    color: MyColors.black,
+                    fontFamily: "Netflix_Sans",
+                    fontWeight: FontWeight.w700,
+                  ),
+                ),
+                icon: const Icon(Icons.fastfood_outlined),
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: MyColors.white,
+                ),
+              )),
+              // Tab(
+              //   child: Row(
+              //     mainAxisAlignment: MainAxisAlignment.center,
+              //     children: [
+              //       Icon(Icons.abc),
+              //       Text('data'),
+              //     ],
+              //   ),
+              // ),
+            ]),
       ),
     );
   }
